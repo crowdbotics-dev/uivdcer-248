9e05ea2d6eca433deb392562cde3ee39bffd4dfd
crowdbotics-dev <engineering@crowdbotics.com>
2024-03-27 11:21:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Hnnjd(models.Model):
+    "Generated Model"
+    eyeue = models.BigIntegerField()
+    gdgeyy = models.BigIntegerField()
